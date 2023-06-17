37a0e4add5f381ba63068f872a6c75fb10143c77
Viraj Agharkar <viraj@quolam.co>
2023-06-18 03:25:15 +0530

    prettier changes

--- frontend/src/pages/customer/index.js
+++ frontend/src/pages/customer/index.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import Form from "./form";
 import CustomerTable from "./customer-table";
-  
-  const Cutomer = () => {
-    const [ isUpdate , setIsUpdate] = useState(false);
-    return (
-      <>
-        <Form isUpdate = {isUpdate} setIsUpdate={setIsUpdate}/>
-        <CustomerTable isUpdate = {isUpdate} setIsUpdate={setIsUpdate}/>
-      </>
-    );
-  };
-  
-  export default Cutomer;
-  
+
+const Cutomer = () => {
+  const [isUpdate, setIsUpdate] = useState(false);
+  return (
+    <>
+      <Form isUpdate={isUpdate} setIsUpdate={setIsUpdate} />
+      <CustomerTable isUpdate={isUpdate} setIsUpdate={setIsUpdate} />
+    </>
+  );
+};
+
+export default Cutomer;
